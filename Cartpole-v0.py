6f7207dddb5dbd57e02078837370fafadad160ec
Manik Rana <manikrana54@gmail.com>
2018-12-28 19:48:51 +0530

    Update Cartpole-v0.py
    implemented a simple neural network to determine the pole's step

--- Cartpole-v0.py
+++ Cartpole-v0.py
@@ -1,17 +1,46 @@
 import gym
+import tensorflow as tf
+import numpy as np
 
+num_inputs = 4
+num_hidden = 4
+num_outputs = 1  # probability to go left
+
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32, shape=[None, num_inputs])
+
+# neural net
+hidden_layer_one = tf.layers.dense(
+    X, num_hidden, activation=tf.nn.relu, kernel_initializer=initializer)
+hidden_layer_two = tf.layers.dense(hidden_layer_one, num_hidden,
+                                   activation=tf.nn.relu, kernel_initializer=initializer)
+output_layer = tf.layers.dense(hidden_layer_two, num_outputs,
+                               activation=tf.nn.relu, kernel_initializer=initializer)
+
+probabilities = tf.concat(axis=1, values=[output_layer, 1-output_layer])
+action = tf.multinomial(probabilities, num_samples=1)
+
+init = tf.global_variables_initializer()
+
+epi = 50
+step_limit = 500
 env = gym.make('CartPole-v1')
+avg_steps = []
 
-observation = env.reset()
+with tf.Session() as sess:
+    init.run()
 
-for t in range(1000):
-    env.render()
+    for i_episode in range(epi):
+        obs = env.reset()
 
-    position, velocity, pole_angle, angular_velocity = observation
-    # basic policy
-    if pole_angle > 0:  # turn right if pole goes left
-        action = 1
-    else:
-        action = 0
+        for step in range(step_limit):
+            action_val = action.eval(feed_dict={X: obs.reshape(1, num_inputs)})
+            obs, reward, done, info = env.step(action_val[0][0])
 
-    observation, reward, done, info = env.step(action)
+            if done:
+                avg_steps.append(step)
+                print('DONE AFTER {} STEPS'.format(step))
+                break
+print("AFTER {} EPISODES, AVERAGE STEPS PER GAME WAS {}".format(epi, np.mean(avg_steps)))
+env.close()
